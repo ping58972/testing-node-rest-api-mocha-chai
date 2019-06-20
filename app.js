87f816935a4b5083d984ad5cbe094de8919fd2b8
ping58972 <ping58972@gmail.com>
2019-06-20 18:44:24 -0500

    deployment to heroku

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const uuidv4 = require('uuid/v4');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 const feedRoutes = require('./routes/feed');
 const authRoutes = require('./routes/auth');
 
 const app = express();
 
-const MONGODB_URI = 'mongodb+srv://ping:pink58972@cluster0-5aiyx.mongodb.net/rest-api-messages?retryWrites=true';
+//const MONGODB_URI = 'mongodb+srv://ping:pink58972@cluster0-5aiyx.mongodb.net/rest-api-messages?retryWrites=true';
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-5aiyx.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+
 
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -49,6 +55,12 @@ app.use((req, res, next) => {
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes); 
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', {stream: accessLogStream}));
+
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
@@ -61,10 +73,10 @@ app.use((error, req, res, next) => {
 });
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true }).then(result => {
-    const server = app.listen(8080);
+    const server = app.listen(process.env.PORT || 8080);
     const io = require('./socket').init(server);
     io.on('connection', socket => {
         console.log(' Client connected');
     });
-    console.log("connected to 8080");
+    console.log(`connected to https: ${process.env.PORT || 8080}`);
 }).catch(err=>console.log(err));
